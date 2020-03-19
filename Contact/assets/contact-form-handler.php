d1f8da3f73745eff7570b3e479e62cfe40ec2028
Muhammad Ali <54961110+moebe-dev@users.noreply.github.com>
2020-03-18 21:41:42 -0500

    Adding HTML PHP CSS
    Pushing Contact Page.

---
+++ Contact/assets/contact-form-handler.php
@@ -0,0 +1,22 @@
+<?php 
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+$email_form = 'trailbooksupp@gmail.com';
+
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $name.\n"."User Email: $visitor_email.\n"."User Message: $message.\n";
+
+    $to = "trailbooksupp@gmail.com"
+
+    $headers = "Form: $email_form \r\n";
+
+    $headers .= "Reply-To : $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html");
+
+?>
